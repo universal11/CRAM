71a4f3e50dbf690fee08399b8edb681aa2f26b2a
Marc Martinez <universal11@Marcs-MacBook-Pro.local>
2014-05-11 19:01:00 -0700

    test

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CRAM
+//
+//  Created by Marc Martinez on 5/11/14.
+//  Copyright (c) 2014 the universal framework. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
